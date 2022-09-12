4ee9263af9bdfc4cdd40657b97aef035794ca85c
Kweayon  Clark <Kweayon@gmail.com>
2022-09-11 22:20:51 -0500

    Update
    We learned to command the computer how to add numbers! Baby steps!

---
+++ review/sum_int_review.cpp
@@ -0,0 +1,17 @@
+// Sum two inputs
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int input1, input2, sum;
+    cout << "Input a number\n";
+    cin >> input1;
+    // cout << "Number selected\n" << input1 * input1 << endl;
+    cout << "Input another number\n";
+    cin >> input2;
+    cout << input1<<"+"<<input2<<"="<<input1 + input2 << endl;
+
+    return 0;
+}
