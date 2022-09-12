021fa0d649688ce03de7e79fb22675912c4408e9
Kweayon  Clark <Kweayon@gmail.com>
2022-09-11 22:37:40 -0500

    Update
    Made some connections to python and dart and sql

--- sum_int.cpp
+++ sum_int.cpp
@@ -1,10 +1,14 @@
-// Adding Two Integers
-#include <iostream>
+// Add Two Numbers
+
+#include <iostream> // Line ran before the program is compiled
+using namespace std; // similar to importing from dart and python, allows for shorter syntax
 
 int main(){
-    int input1, input2, sum;
-    std::cout << "Enter first integer\n";
-    std::cin >> input1;
-    std::cout << "Input 1:" << input1 << std::endl;
+    int input1, input2, sum; // integer variables
+    cout << "Input a number\n"; // similar to stdout.write() or print
+    cin >> input1; // similar to stdin.readLineSync()
+    cout << "Input another number\n";
+    cin >> input2;
+    cout << input1 << "+" << input2 << "=" << input1 + input2 << endl;
     return 0;
 }
