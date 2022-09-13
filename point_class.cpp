3c99e36597705be1ed43f1ef62b32a8f814a3016
Kweayon  Clark <Kweayon@gmail.com>
2022-09-13 12:10:57 -0500

    Update

---
+++ point_class.cpp
@@ -0,0 +1,57 @@
+// Class Point(x,y) euclidean distance
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Point{
+    public:
+      double x, y;
+      Point(double x, double y){
+        this->x = x;
+        this->y = y;
+      }
+      static double euclideanDistance(Point p1, Point p2){
+        double distance = sqrt(_pow(p2.x - p1.x,2) + _pow(p2.y-p1.y,2));
+        return distance;
+      }
+
+      Point operator+(Point& p){
+        Point p1(this->x + p.x, this->y + p.y);
+        return p1;
+      }
+       Point operator*(Point& p){
+        Point p1(this->x * p.x, this->y * p.y);
+        return p1;
+      }
+
+       Point operator/(Point& p){
+        Point p1(this->x / p.x, this->y / p.y);
+        return p1;
+      }
+
+       Point operator-(Point& p){
+        Point p1(this->x - p.x, this->y - p.y);
+        return p1;
+      }
+      
+    private:
+      static double _pow(double n, int e){
+        if(e == 0){
+            return 1;
+        }
+        return n * _pow(n, e-1);
+      }
+
+};
+
+int main(){
+    Point p1(11,7);
+    Point p2(7,9);
+    Point p3 = p1 / p2;
+    cout << "x = " << p1.x << " y = " << p1.y << endl;
+    cout << "The distance of p1 and p2 = " << Point::euclideanDistance(p1,p2) << endl;
+    cout << "p1 / p2 = " << "Point(" << p3.x << ", " << p3.y <<")" << endl;
+    return 0;
+}
--- review/classes.cpp
+++ review/classes.cpp
@@ -7,10 +7,19 @@ using namespace std;
 class Point{
     public:
       double x, y;
-      Point(double val1, double val2){
-        x = val1;
-        y = val2;
+      Point(double x, double y){
+        this -> x = x;
+        this -> y = y;
       }
+      Point operator+(Point& p){
+        Point p1(this->x + p.x, this->y + p.y);
+        return p1;
+      }
+      static double euclideanDistance(Point p1, Point p2){
+        double distance = sqrt(_pow(p2.x - p1.x,2) + _pow(p2.y - p1.y,2));
+        return distance;
+      }
+
     private:
       static double _pow(double n, int e){
         if(e == 0){
@@ -18,16 +27,16 @@ class Point{
         }
         return n * _pow(n, e - 1);
       }
-
-      static double euclideanDistance(Point p1, Point p2){
-        double distance = sqrt(_pow(p2.x - p1.x,2) + _pow(p2.y - p1.y,2));
-      }
-
 };
 
 int main(){
     Point p1(3,4);
     Point p2(7,9);
+    Point p3 = p1 + p2;
+
+    cout << "x = " << p3.x << " y = " << p3.y << endl;
+    cout << Point::euclideanDistance(p1,p3) << endl;
+    
     return 0;
 }
 
