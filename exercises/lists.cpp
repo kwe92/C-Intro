5a69ee58f67431927b10996eb0a0d90d99c65f2f
Kweayon  Clark <Kweayon@gmail.com>
2022-09-15 22:15:44 -0500

    Update
    Added pointers

--- exercises/lists.cpp
+++ exercises/lists.cpp
@@ -3,18 +3,8 @@
 
 using namespace std;
 
-double pow2(double n, int e = 2){
-    if(e == 0){
-        return 1;
-    }else {
-        return n * pow2(n, e-1);
-    }
-}
 
-
-/*
-
-double* map(double (*func)(double n, int e), double arr[]){
+double *transform(double (*func)(double n), double arr[]){
     int length = sizeof(arr) / sizeof(double);
     double newArr[length];
     for(int i = 0; i < length; i++){
@@ -23,17 +13,53 @@ double* map(double (*func)(double n, int e), double arr[]){
     return newArr;
 }
 
-*/
+double times2(double n){
+    return n * 2;
+}
+
+
 
 int main(){
     double listDouble[3] = {2.3, 5.4, 6.7};
-    cout << listDouble << endl;
-    cout << &listDouble << endl;
-    cout << sizeof(double) << endl;// returns the size of a single type or element
-    cout << sizeof(listDouble) << endl; // returns the cumulative sum of objects in the array
-    for(int i = 0; i < (sizeof(listDouble) / sizeof(double)); i++){
-        cout << pow(listDouble[i], 2) << endl;
+    double transformedList[3];
+    double easyway[3];
+    for(int i = 0; i < (sizeof(listDouble) / sizeof(double)); i++)
+    {
+        transformedList[i] = *(transform(times2, listDouble + i));
+    }
+     for(int i = 0; i < (sizeof(listDouble) / sizeof(double)); i++){
+        cout << transformedList[i] << endl;
+    }
+    for(int i = 0; i < 3; i++){
+        easyway[i] = times2(listDouble[i]);
+    }
+     for(int i = 0; i < 3; i++){
+        cout << "easy way: " << easyway[i] << endl;
     }
     return 0;
-    
 }
+    //cout << listDouble << endl;
+    //cout << &listDouble << endl;
+    //cout << sizeof(double) << endl;// returns the size of a single type or element
+    //cout << sizeof(listDouble) << endl; // returns the cumulative sum of objects in the array
+   
+    /*
+    
+    for(int i = 0; i < (sizeof(newList) / sizeof(double)); i++){
+        cout << newList[i] << endl;
+    }
+    
+    */
+
+    /*
+    
+    double pow2(double n, int e = 2){
+    if(e == 0){
+        return 1;
+    }else {
+        return n * pow2(n, e-1);
+    }
+}
+
+    */
+    
---
+++ exercises/using_pointers.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+double *map(double (func)(double n), double arr[]){
+    int length = sizeof(arr) / sizeof(double);
+    double newArr[length];
+    for(int i = 0; i < length; i++){
+        newArr[i] = func(arr[i]);
+    }
+    return newArr;
+}
+
+double times2(double n){
+    return n * 2;
+}
+
+ 
+int main () {
+   // an array with 5 elements.
+   double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+   double *ptr;
+   double *p;
+   // p = balance;
+   ptr = map(times2,balance);
+   p = ptr;
+ 
+   // output each array element's value 
+   cout << "Array values using pointer " << endl;
+   
+   for ( int i = 0; i < 5; i++ ) {
+      cout << "*(p + " << i << ") : ";
+      cout << *map(times2,balance + i) << endl;
+   }
+   cout << "Array values using balance as address " << endl;
+   
+   for ( int i = 0; i < 5; i++ ) {
+      cout << "*(balance + " << i << ") : ";
+      cout << *(balance + i) << endl;
+   }
+ 
+   return 0;
+}
