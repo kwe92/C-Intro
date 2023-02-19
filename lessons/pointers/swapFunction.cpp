c365cd24c84f0ba3f2c42bf8a99064d91b780d62
Kweayon  Clark <Kweayon@gmail.com>
2023-02-19 00:24:04 -0600

    Update

---
+++ lessons/pointers/swapFunction.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Structures (struct keyword)
+// - A way of grouping related variables of any type into one place
+
+// Creating a Structure
+// Unamed Structures (Objects)
+// 	- Unnamed structs object body are suffixed with a name
+// 	- Unnamed structs are mutable objects with explicitly defined properties initalized as empty
+
+// Named Structures (Intefaces / Types)
+// 	- Named structs object body are prefixed with a name
+// Named structs are Interfaces / Types
+
+// Swap Name Function
+// 	- Write a function in C++ that swaps someones names;
+
+void swapName(string &fname, string &lname)
+{
+	string temp = fname;
+	fname = lname;
+	lname = temp;
+}
+
+int main()
+{
+
+	struct
+	{
+		string fname;
+		string lname;
+	} hero;
+
+	struct Villain
+	{
+		string name;
+		string show;
+	};
+
+	hero.fname = "Gon";
+	hero.lname = "Freecss";
+
+	Villain villain;
+	villain.name = "Meruem";
+	villain.show = "Hunter x Hunter";
+
+	cout << "First Name: " << hero.fname << "\n"
+		 << "Last Name: " << hero.lname << endl;
+
+	cout << "Villain Name: " << villain.name << "\n"
+		 << "Show: " << villain.show << endl;
+	swapName(hero.fname, hero.lname);
+	cout
+		<< "First Name: " << hero.fname << "\n"
+		<< "Last Name: " << hero.lname << endl;
+
+	return 0;
+}
